fd3f0b09feee9474f388e0f4538be22c751a9775
DESKTOP-QV1R0KT\Administrator <1248694620@qq.com>
2019-04-26 22:43:11 +0800

    2019.04.26 22:43

--- Maker/View/UI/UserControlDialog/MyBlogDialog.xaml.cs
+++ Maker/View/UI/UserControlDialog/MyBlogDialog.xaml.cs
@@ -21,5 +21,18 @@ namespace Maker.View.UI.UserControlDialog
         {
             mw.RemoveDialog();
         }
+
+        private void Border_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            //System.Diagnostics.Process.Start(@"E:\Sharer\Maker\Maker\bin\Debug\matrix uploader\dfu-util.exe",
+            //    "dfu-util -v -d 0203:0100,0203:0003 -t 2048 -a 0 -R -D \" "+ @"E:\Sharer\Maker\Maker\bin\Debug\matrix uploader\MatrixFW 0.1.3.3b 4-25-1.mxfw" + "\"");
+         
+            System.Diagnostics.Process.Start(@"E:\Sharer\Maker\Maker\bin\Debug\matrixuploader\Matrix Firmware Uploader.bat",
+              "\"E:\\Sharer\\Maker\\Maker\\bin\\Debug\\matrixuploader\\MatrixFW 0.1.3.3 b4-25-1.mxfw\"");
+
+            //细节优化。
+            //如是否要再次确认
+            //echo Make sure Matrix is pluged in. Press Any Key to continue.
+        }
     }
 }
---
+++ Maker/bin/Debug/matrixuploader/Matrix Firmware Uploader - 副本 (2).bat
@@ -0,0 +1,25 @@
+@echo off
+cd /d "%~dp0"
+
+echo Matrix Firmware Uploader V1.00
+echo.
+
+if "%1" == "" (
+echo No file detected, please drag^&drop .mxfw file on to the exe file. Press any key to exit. 
+pause >nul
+exit
+)
+
+if NOT "%~x1" == ".mxfw" (
+echo This isn't a Matrix Firmware file ^(.mxfw^). Press any key to exit. 
+pause >nul
+exit
+)
+
+
+echo Make sure Matrix is pluged in. Press Any Key to continue.
+pause >nul
+echo.
+
+dfu-util -v -d 0203:0100,0203:0003 -t 2048 -a 0 -R -D "%~%1"
+pause >nul
